bda685de0e0c409837f155add87fa1840bf53b76
Ashis <developer.ashis99@gmail.com>
2023-12-02 02:12:31 +0530

    Added Welcome Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import WelcomePage from './components/WelcomePage';
+import { Router,Routes,Route } from 'react-router-dom';
 
 function App() {
+  let user = JSON.parse(localStorage.getItem("user"));
+  if(user){
+    console.log(`Redirect ${user.name} to Dashboard!`);
+  }
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element= {<WelcomePage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
